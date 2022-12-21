a2a9c872db4b54567d393308a95061556b94cf91
Sk-sakil-ali <121190073+Sk-sakil-ali@users.noreply.github.com>
2022-12-21 23:51:21 +0530

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+    string name, email;
+    int age;
+    cout<<"MAY I KNOW YOUR NAME PLEASE ";
+    cin>>name;
+    cout<<endl<<"HEY! "<<name<<" NICE TO MEET YOU "<<endl;
+    cout<<"HOPE YOU ARE ABSOLUTELY FINE"<<endl;
+    cout<<"CAN I KNOW YOUR EMAIL SO THAT WE CAN SEND YOU EXCLUSIVE NOTIFICATIONS AND REWARDS "<<endl;
+    cin>>email;
+    cout<<endl<<"GREAT! YOU ARE NOW PART OF OUR FAMILY "<<endl;
+    cout<<"Can I know our age to give you persionalized suggestions "<<endl;
+    cin>>age;
+    cout<<endl<<"Thanks for our patience we will try our best to provide you he best services  ";
+}
